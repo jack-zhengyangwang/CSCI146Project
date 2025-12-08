33857e18fded5c156edc32e0d6b2baa0c3656a5c
jack-zhengyangwang <jackzhengyangwang0424@gmail.com>
2025-12-08 13:21:14 -0800

    Update project.py

--- project.py
+++ project.py
@@ -1,27 +1,7 @@
-# IMPORTANT: RUN THIS CELL IN ORDER TO IMPORT YOUR KAGGLE DATA SOURCES,
-# THEN FEEL FREE TO DELETE THIS CELL.
-# NOTE: THIS NOTEBOOK ENVIRONMENT DIFFERS FROM KAGGLE'S PYTHON
-# ENVIRONMENT SO THERE MAY BE MISSING LIBRARIES USED BY YOUR
-# NOTEBOOK.
 import kagglehub
-visualize25_valorant_pro_matches_full_data_path = kagglehub.dataset_download('visualize25/valorant-pro-matches-full-data')
-
-print('Data source import complete.')
-
-
-import numpy as np
 import pandas as pd
-import sqlite3 as sql
-import matplotlib.pyplot as plt
-import random
-import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from sklearn.metrics import accuracy_score, confusion_matrix, log_loss, mean_squared_error, classification_report, ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.impute import SimpleImputer
+# Download latest version
+path = kagglehub.dataset_download("visualize25/valorant-pro-matches-full-data")
 
-import datetime
+print("Path to dataset files:", path)
 
